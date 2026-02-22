f9315f6bc6e9c294d2ff7293d044ca5df4698938
Yan Kazymyr <kazymyr.yan@gmail.com>
2026-02-22 17:17:32 +0200

    Solution

--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,29 +1,95 @@
 import { PeopleFilters } from './PeopleFilters';
 import { Loader } from './Loader';
 import { PeopleTable } from './PeopleTable';
+import { useEffect, useState } from 'react';
+import { Person } from '../types';
+import { getPeople } from '../api';
+import { useSearchParams } from 'react-router-dom';
 
 export const PeoplePage = () => {
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const [searchParams] = useSearchParams();
+  const sex = searchParams.get('sex');
+  const query = searchParams.get('query') || '';
+  const centuries = searchParams.getAll('centuries');
+  const filteredPeople = people
+    .filter(person => !sex || person.sex === sex)
+    .filter(
+      person =>
+        person.name.toLowerCase().includes(query.toLowerCase()) ||
+        person.motherName?.toLowerCase().includes(query.toLowerCase()) ||
+        person.fatherName?.toLowerCase().includes(query.toLowerCase()),
+    )
+    .filter(
+      person =>
+        !centuries.length ||
+        centuries.includes(String(Math.ceil(person.born / 100))),
+    );
+
+  const sortField = searchParams.get('sort');
+  const sortOrder = searchParams.get('order');
+  const sortedPeople = [...filteredPeople].sort((a, b) => {
+    if (!sortField) {
+      return 0;
+    }
+
+    let result = 0;
+
+    if (sortField === 'name' || sortField === 'sex') {
+      result = a[sortField].localeCompare(b[sortField]);
+    } else if (sortField === 'born' || sortField === 'died') {
+      result = a[sortField] - b[sortField];
+    }
+
+    return sortOrder === 'desc' ? -result : result;
+  });
+
+  useEffect(() => {
+    setIsLoading(true);
+    setIsError(false);
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => setIsError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <>
       <h1 className="title">People Page</h1>
 
       <div className="block">
         <div className="columns is-desktop is-flex-direction-row-reverse">
-          <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
-          </div>
+          {!isLoading && !isError && people.length > 0 && (
+            <div className="column is-7-tablet is-narrow-desktop">
+              <PeopleFilters />
+            </div>
+          )}
 
           <div className="column">
             <div className="box table-container">
-              <Loader />
+              {isLoading && <Loader />}
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+              {!isLoading && isError && (
+                <p data-cy="peopleLoadingError">Something went wrong</p>
+              )}
 
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
+              {!isLoading && !isError && people.length === 0 && (
+                <p data-cy="noPeopleMessage">
+                  There are no people on the server
+                </p>
+              )}
 
-              <p>There are no people matching the current search criteria</p>
+              {people.length > 0 && filteredPeople.length === 0 && (
+                <p>There are no people matching the current search criteria</p>
+              )}
 
-              <PeopleTable />
+              {!isLoading && !isError && people.length > 0 && (
+                <PeopleTable people={sortedPeople} />
+              )}
             </div>
           </div>
         </div>
